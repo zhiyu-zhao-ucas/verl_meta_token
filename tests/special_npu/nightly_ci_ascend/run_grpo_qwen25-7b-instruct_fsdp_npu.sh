72207cef369b00a9ee23b5fced1ecc328a41b002
daikang6 <165463455+daikang6@users.noreply.github.com>
2026-03-09 10:16:45 +0800

    [ci] feat: add npu nightly ci (#5225)
    ### What does this PR do?
    
    add npu nightly ci (more will be added in the future)
    
    ### Checklist Before Starting
    
    - [ ] Search for similar PRs. Paste at least one query link here: ...
    - [ ] Format the PR title as `[{modules}] {type}: {description}` (This
    will be checked by the CI)
    - `{modules}` include `fsdp`, `megatron`, `veomni`, `sglang`, `vllm`,
    `rollout`, `trainer`, `ci`, `training_utils`, `recipe`, `hardware`,
    `deployment`, `ray`, `worker`, `single_controller`, `misc`, `perf`,
    `model`, `algo`, `env`, `tool`, `ckpt`, `doc`, `data`, `cfg`, `reward`
    - If this PR involves multiple modules, separate them with `,` like
    `[megatron, fsdp, doc]`
      - `{type}` is in `feat`, `fix`, `refactor`, `chore`, `test`
    - If this PR breaks any API (CLI arguments, config, function signature,
    etc.), add `[BREAKING]` to the beginning of the title.
      - Example: `[BREAKING][fsdp, megatron] feat: dynamic batching`
    
    ### Test
    
    > For changes that can not be tested by CI (e.g., algorithm
    implementation, new model support), validate by experiment(s) and show
    results like training curve plots, evaluation results, etc.
    
    ### API and Usage Example
    
    > Demonstrate how the API changes if any, and provide usage example(s)
    if possible.
    
    ```python
    # Add code snippet or script demonstrating how to use this
    ```
    
    ### Design & Code Changes
    
    > Demonstrate the high-level design if this PR is complex, and list the
    specific changes.
    
    ### Checklist Before Submitting
    
    > [!IMPORTANT]
    > Please check all the following items before requesting a review,
    otherwise the reviewer might deprioritize this PR for review.
    
    - [ ] Read the [Contribute
    Guide](https://github.com/volcengine/verl/blob/main/CONTRIBUTING.md).
    - [ ] Apply [pre-commit
    checks](https://github.com/volcengine/verl/blob/main/CONTRIBUTING.md#code-linting-and-formatting):
    `pre-commit install && pre-commit run --all-files --show-diff-on-failure
    --color=always`
    - [ ] Add / Update [the
    documentation](https://github.com/volcengine/verl/tree/main/docs).
    - [ ] Add unit or end-to-end test(s) to [the CI
    workflow](https://github.com/volcengine/verl/tree/main/.github/workflows)
    to cover all the code. If not feasible, explain why: ...
    - [ ] Once your PR is ready for CI, send a message in [the `ci-request`
    channel](https://verl-project.slack.com/archives/C091TCESWB1) in [the
    `verl` Slack
    workspace](https://join.slack.com/t/verl-project/shared_invite/zt-3855yhg8g-CTkqXu~hKojPCmo7k_yXTQ).
    (If not accessible, please try [the Feishu group
    (飞书群)](https://applink.larkoffice.com/client/chat/chatter/add_by_link?link_token=772jd4f1-cd91-441e-a820-498c6614126a).)
    - [ ] If your PR is related to the `recipe` submodule, please also
    update the reference to the submodule commit via `git submodule update
    --remote` or `cd recipe && git pull origin main`.

--- tests/special_npu/nightly_ci_ascend/run_grpo_qwen25-7b-instruct_fsdp_npu.sh
+++ tests/special_npu/nightly_ci_ascend/run_grpo_qwen25-7b-instruct_fsdp_npu.sh
@@ -1,15 +1,21 @@
 set -x
 
+# Some models are optimized by vllm ascend. While in some case, e.g. rlhf training, 
+# the optimized model may not be suitable. In this case, set this value to 0 to disable the optimized model.
+
+MODEL_ID=${MODEL_ID:-Qwen/Qwen2.5-7B-Instruct}
+MODEL_PATH=${MODEL_PATH:-${HOME}/.cache/models/${MODEL_ID}}
+
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=$HOME/data/gsm8k/train.parquet \
     data.val_files=$HOME/data/gsm8k/test.parquet \
-    data.train_batch_size=1024 \
+    data.train_batch_size=32 \
     data.max_prompt_length=1024 \
     data.max_response_length=1024 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
-    actor_rollout_ref.model.path=Qwen/Qwen2.5-7B-Instruct \
+    actor_rollout_ref.model.path="${MODEL_PATH}" \
     actor_rollout_ref.actor.optim.lr=5e-8 \
     actor_rollout_ref.model.use_remove_padding=False \
     actor_rollout_ref.actor.ppo_mini_batch_size=32 \
@@ -24,19 +30,20 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.3 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
     actor_rollout_ref.rollout.n=5 \
     actor_rollout_ref.rollout.checkpoint_engine.update_weights_bucket_megabytes=4096 \
     actor_rollout_ref.rollout.enable_chunked_prefill=False \
+    actor_rollout_ref.rollout.calculate_log_probs=True \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
     trainer.logger=console \
     trainer.project_name='verl_grpo_example_gsm8k' \
-    trainer.experiment_name='qwen2_5_7b_function_rm' \
-    trainer.n_gpus_per_node=16 \
+    trainer.experiment_name='qwen2_5_7b_instruct_fsdp' \
+    trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
-    trainer.test_freq=5 \
-    trainer.total_epochs=5 $@
+    trainer.test_freq=-1 \
+    trainer.total_training_steps=15 $@
